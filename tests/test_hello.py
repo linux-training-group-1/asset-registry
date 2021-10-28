6b5d9fbddbdabdf84c79f86c92655b6433a3b29a
rumesh <rumesh.m.s16@gmail.com>
2021-10-28 18:28:00 +0530

    adding integration test using selenium

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -4,3 +4,20 @@ def test_hello():
 
 def test_hi():
     assert 2 + 2 == 4
+
+
+import time
+from selenium import webdriver
+
+
+def test_integration_using_selenium():
+    driver = webdriver.Chrome()
+    driver.get('https://www.google.com/')
+    print(driver.title)
+    time.sleep(5)
+    search_box = driver.find_element_by_name('q')
+    search_box.send_keys('ChromeDriver')
+    search_box.submit()
+    time.sleep(5)
+    print(driver.title)
+    driver.quit()
