605ab5a9bd6112df36a8c7a1096b89e03e0c06a0
rumesh <rumesh.m.s16@gmail.com>
2021-11-18 08:25:03 +0530

    deleted comments

--- utils/mysql_db.py
+++ utils/mysql_db.py
@@ -22,16 +22,6 @@ def get_connection():
 
 def check_credentials(username, password, admin=True):
     conn = get_connection()
-    # with connection:
-    #     with connection.cursor() as cursor:
-    #         # Create a new record
-    #         sql = "INSERT INTO `users` (`email`, `password`) VALUES (%s, %s)"
-    #         cursor.execute(sql, ('webmaster@python.org', 'very-secret'))
-    #
-    # # connection is not autocommit by default. So you must commit to save
-    # # your changes.
-    # connection.commit()
-
     with conn.cursor() as cursor:
         # Read a single record
         # TODO: check salt + hashed password
