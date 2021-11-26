7e916baa1f34a5d41e7a67e89bb94464daa2d55d
rumesh <rumesh.m.s16@gmail.com>
2021-11-27 02:02:25 +0530

    test ci

--- utils/replace_tag.py
+++ utils/replace_tag.py
@@ -1,14 +1,13 @@
 import sys
-
+import re
 file_name = sys.argv[1]
-old_tag = sys.argv[2]
-new_tag = sys.argv[3]
+new_tag = sys.argv[2]
 
 f = open(file_name, 'r')
 file_data = f.read()
 f.close()
 
-new_data = file_data.replace(old_tag, new_tag)
+new_data = re.sub('/asset-app:.*', new_tag, file_data, flags=re.M)
 
 f = open(file_name, 'w')
 f.write(new_data)
