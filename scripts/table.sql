40f019b66e212432e53bd4ff24a86978d7928b15
rumesh <rumesh.m.s16@gmail.com>
2021-11-26 13:16:42 +0530

    updated sql file

--- scripts/table.sql
+++ scripts/table.sql
@@ -1,25 +1,21 @@
-CREATE database if not exists asset_app;
-USE asset_app;
-CREATE TABLE if not exists `asset` (
-                                       `name` VARCHAR(255),
-                                       `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-                                       `owner` VARCHAR(255),
-                                       `description` VARCHAR(1000),
-                                       `location` VARCHAR(1000),
-                                       `criticality` enum('Critical','Medium','Low') DEFAULT 'Low',
-                                        FULLTEXT ftidx (`name`)
-) ENGINE=InnoDB;
+CREATE database if not exists asset_db;
+USE asset_db;
+create table asset
+(
+    asset_id    INT           NOT NULL AUTO_INCREMENT,
+    name        VARCHAR(50)   NOT NULL unique,
+    owner       VARCHAR(50)   NOT NULL,
+    description VARCHAR(1024) NOT NULL,
+    location    VARCHAR(50)   NOT NULL,
+    criticality VARCHAR(10)   NOT NULL,
+    PRIMARY KEY (asset_id)
+);
 
-
-CREATE TABLE if not exists `user` (
-                                      `username` VARCHAR(255),
-                                      `name` varchar(255),
-                                      `password` VARCHAR(1024),
-                                      `salt` VARCHAR(1024),
-                                      `admin` boolean DEFAULT false,
-                                      UNIQUE KEY `idexun` (`username`) USING BTREE,
-                                      PRIMARY KEY (`username`)
-) ENGINE=InnoDB;
-
-INSERT INTO asset_app.user (username, name, password, salt, admin)
-VALUES ('bob', 'Bob Anderson', 'password', 'mysalt', 1);
+create table user
+(
+    user_id  int          not null primary key auto_increment,
+    username varchar(128) not null unique,
+    pwd      varchar(128) not null
+);
+create user if not exists 'asset-app'@'%' identified by 'akjgSDf#69';
+grant all privileges on asset_db.* to 'asset-app'@'%';
