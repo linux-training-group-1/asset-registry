9fca684b0f7328dcae09e34809a7f4ba2ffbf314
rumesh <rumesh.m.s16@gmail.com>
2021-12-25 17:22:27 +0530

    formatted

--- tests/test_app.py
+++ tests/test_app.py
@@ -12,15 +12,17 @@ username = 'admin'
 password = 'password'
 service = Service(ChromeDriverManager().install())
 
+
 def admin_login(driver):
     driver.get(base_url)
     driver.find_element(By.NAME, 'username').send_keys(username)
     driver.find_element(By.NAME, 'password').send_keys(password)
     driver.find_element(By.NAME, 'submit').click()
 
+
 class TestLogin:
     def test_login_success(self):
-        driver = webdriver.Chrome(service = service)
+        driver = webdriver.Chrome(service=service)
         admin_login(driver)
         assert (base_url + "dashboard") in driver.current_url
         assert "Asset App" in driver.page_source
@@ -28,7 +30,7 @@ class TestLogin:
         driver.quit()
 
     def test_login_fail(self):
-        driver = webdriver.Chrome(service = service)
+        driver = webdriver.Chrome(service=service)
         driver.get(base_url)
         driver.find_element(By.NAME, 'username').send_keys('john')
         driver.find_element(By.NAME, 'password').send_keys('pass')
@@ -37,24 +39,26 @@ class TestLogin:
         driver.quit()
 
     def test_logout_success(self):
-        driver = webdriver.Chrome(service = service)
+        driver = webdriver.Chrome(service=service)
         admin_login(driver)
-        driver.get(base_url+'logout')
+        driver.get(base_url + 'logout')
         assert "You have been logged out" in driver.page_source
         driver.quit()
 
+
 class TestListAssets:
     def test_list_assets_success(self):
-        driver = webdriver.Chrome(service = service)
+        driver = webdriver.Chrome(service=service)
         admin_login(driver)
         driver.find_element(By.LINK_TEXT, "LIST ASSETS").click()
         assert (base_url + "list-assets") in driver.current_url
-        assert "ID","Name" in driver.find_element(By.ID, "list-assets-page")
+        assert "ID", "Name" in driver.find_element(By.ID, "list-assets-page")
         driver.quit()
+
 
 class TestAddAssets:
     def test_add_asset_success(self):
-        driver = webdriver.Chrome(service = service)
+        driver = webdriver.Chrome(service=service)
         admin_login(driver)
         driver.find_element(By.LINK_TEXT, "ADD ASSETS").click()
         assert (base_url + "add-asset") in driver.current_url
@@ -65,12 +69,13 @@ class TestAddAssets:
         Select(driver.find_element(By.NAME, 'assetCriticality')).select_by_value('medium')
         driver.find_element(By.NAME, 'submit').click()
         assert (base_url + "list-assets") in driver.current_url
-        assert "Mobile Phone","John Doe" in driver.find_element(By.ID, "list-assets-page")
+        assert "Mobile Phone", "John Doe" in driver.find_element(By.ID, "list-assets-page")
         driver.quit()
+
 
 class TestEditAssets:
     def test_edit_asset_success(self):
-        driver = webdriver.Chrome(service = service)
+        driver = webdriver.Chrome(service=service)
         admin_login(driver)
         driver.find_element(By.LINK_TEXT, "LIST ASSETS").click()
         driver.find_elements(By.ID, 'edit-asset-btn').pop().click()
@@ -83,7 +88,7 @@ class TestEditAssets:
         driver.quit()
 
     def test_delete_asset_success(self):
-        driver = webdriver.Chrome(service = service)
+        driver = webdriver.Chrome(service=service)
         admin_login(driver)
         driver.find_element(By.LINK_TEXT, "LIST ASSETS").click()
         items = driver.find_elements(By.ID, 'edit-asset-btn')
@@ -91,5 +96,5 @@ class TestEditAssets:
         items.pop().click()
         driver.find_element(By.NAME, 'submit_delete').click()
         assert (base_url + "list-assets") in driver.current_url
-        assert (total_assets-1) == len(driver.find_elements(By.ID, "edit-asset-btn"))
+        assert (total_assets - 1) == len(driver.find_elements(By.ID, "edit-asset-btn"))
         driver.quit()
