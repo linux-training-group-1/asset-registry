e9e2db6b8b3c72fe0f71acfa46bc53a4d2e1f30f
rumesh <rumesh.m.s16@gmail.com>
2022-01-10 06:53:41 +0530

    e2e tests - change the url

--- tests/test_app.py
+++ tests/test_app.py
@@ -7,7 +7,7 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
 base_url = os.getenv('STAGING_IP', default='localhost:5000')
-base_url = "http://" + base_url+"/"
+base_url = "http://" + base_url + "/"
 username = 'admin'
 password = 'password'
 service = Service(ChromeDriverManager().install())
