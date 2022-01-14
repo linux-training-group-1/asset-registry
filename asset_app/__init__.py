15aaacb31328965522901896cabda62489bedea8
Indika Wijesinghe <indikawijesinghe243@gmail.com>
2022-01-14 18:19:54 +0530

    fixes font size issues

--- asset_app/__init__.py
+++ asset_app/__init__.py
@@ -8,20 +8,28 @@ from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 import redis
 
-logging.config.fileConfig('logging.conf')
-logger = logging.getLogger('app')
+# logging.config.fileConfig('logging.conf')
+# logger = logging.getLogger('app')
 load_dotenv()
 app = Flask(__name__)
 
 # redis config
 redis_client = redis.Redis(host=os.environ['REDIS_HOST'],port=os.environ['REDIS_PORT'])
 
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://' + os.environ['MYSQL_USER'] + ":" + os.environ[
     'MYSQL_PASSWORD'] + "@" + os.environ['MYSQL_HOST'] + ":" + os.environ['MYSQL_PORT'] + "/" + os.environ['MYSQL_DATABASE']
-app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# app.config['SQLALCHEMY_BINDS']={'one':'mysql+pymysql://' + os.environ['MYSQL_USER'] + ":" + os.environ[
+#     'MYSQL_PASSWORD'] + "@" + os.environ['MYSQL_HOST_1'] + ":" + os.environ['MYSQL_PORT'] + "/" + os.environ['MYSQL_DATABASE'],'two':'mysql+pymysql://' + os.environ['MYSQL_USER'] + ":" + os.environ[
+#     'MYSQL_PASSWORD'] + "@" + os.environ['MYSQL_HOST_2'] + ":" + os.environ['MYSQL_PORT'] + "/" + os.environ['MYSQL_DATABASE']}   
 
 db = SQLAlchemy(app)
+
+app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.config['SQLALCHEMY_POOL_RECYCLE']=30
+
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = "home_page"
