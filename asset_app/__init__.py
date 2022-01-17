8853bfa569c6faededc5ddbff08c8c701ab3d2f8
rumesh <rumesh.m.s16@gmail.com>
2022-01-17 11:18:02 +0530

    refactor the code

--- asset_app/__init__.py
+++ asset_app/__init__.py
@@ -16,18 +16,17 @@ app = Flask(__name__)
 # redis config
 redis_client = redis.Redis(host=os.environ['REDIS_HOST'], port=int(os.environ['REDIS_PORT']))
 
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://' + os.environ['MYSQL_USER'] + ":" + os.environ[
-    'MYSQL_PASSWORD'] + "@" + os.environ['MYSQL_HOST'] + ":" + os.environ['MYSQL_PORT'] + "/" + os.environ['MYSQL_DATABASE']
-
-db = SQLAlchemy(app)
-app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://' + os.environ['MYSQL_USER'] + ":" + \
+                                        os.environ['MYSQL_PASSWORD'] + "@" + os.environ['MYSQL_HOST'] + \
+                                        ":" + os.environ['MYSQL_PORT'] + "/" + os.environ['MYSQL_DATABASE']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_POOL_RECYCLE']=30
-
+app.config['SQLALCHEMY_POOL_RECYCLE'] = 30
+app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
+db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = "home_page"
+
 os.environ["PROMETHEUS_MULTIPROC_DIR"] = "/tmp"
 os.environ["prometheus_multiproc_dir"] = "/tmp"
 from asset_app import routes
